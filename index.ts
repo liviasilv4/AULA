722c5bbe37a867b6b457366be024554e9bd9e38a
livinha <analivinhalilica2103@gmail.com>
2024-10-17 21:52:24 -0300

    meu primeiro commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log("Olá");
+
+//git remote add origin: cria um link ente o repositório local e o repositor no
+//provetor de repositório
+// git add . : adiciona os arquivos no repositório
+//git status: lista os arquivos (adicionados, modificados)
+
+
